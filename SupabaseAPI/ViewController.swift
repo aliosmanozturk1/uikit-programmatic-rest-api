a1a97bd7aa9848f9df8ee3531720ae69627b0484
Ali Osman Öztürk <github@aliosmanozturk.me>
2025-09-25 18:06:01 +0300

    Initial Commit

---
+++ SupabaseAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SupabaseAPI
+//
+//  Created by Ali Osman Öztürk on 25.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
