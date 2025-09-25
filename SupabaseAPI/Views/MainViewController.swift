b43a1ad870134504260f7cc0aac3997bfb5a975b
Ali Osman Öztürk <github@aliosmanozturk.me>
2025-09-25 18:34:06 +0300

    feat: add sign out button to main screen
    Add sign out button to main screen with proper layout constraints
    
      - Add sign out button with blue background and rounded corners
      - Update welcome label positioning to top area
      - Add button tap handler placeholder

--- SupabaseAPI/Views/MainViewController.swift
+++ SupabaseAPI/Views/MainViewController.swift
@@ -4,12 +4,11 @@
 //
 //  Created by Ali Osman Öztürk on 25.09.2025.
 //
-
-
 import UIKit
 
 class MainViewController: UIViewController {
     private var welcomeLabel: UILabel!
+    private var signOutButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,19 +26,36 @@ class MainViewController: UIViewController {
         welcomeLabel.text = "Welcome to RESTfulAPI"
         welcomeLabel.textAlignment = .center
         welcomeLabel.font = .systemFont(ofSize: 24, weight: .bold)
+        
+        signOutButton = UIButton(type: .system)
+        signOutButton.translatesAutoresizingMaskIntoConstraints = false
+        signOutButton.setTitle("Sign Out", for: .normal)
+        signOutButton.setTitleColor(.white, for: .normal)
+        signOutButton.addTarget(self, action: #selector(signOutButtonTapped), for: .touchUpInside)
+        signOutButton.backgroundColor = .systemBlue
+        signOutButton.layer.cornerRadius = 8
+        
         view.addSubview(welcomeLabel)
+        view.addSubview(signOutButton)
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
             welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            welcomeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            welcomeLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            signOutButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            signOutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            signOutButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50)
         ])
     }
     
     private func setupNavigationBar() {
         navigationItem.title = "Main"
         navigationController?.navigationBar.isHidden = false
+    }
+    
+    @objc private func signOutButtonTapped() {
+        
     }
 }
 
