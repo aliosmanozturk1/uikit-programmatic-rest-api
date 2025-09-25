2ad19d255df1cb4f943d9e3e5ca75ff23f078173
Ali Osman Öztürk <github@aliosmanozturk.me>
2025-09-25 18:26:11 +0300

    feat: add main screen with welcome interface
    - Implement MainViewController with welcome label
    - Add proper layout constraints and navigation bar setup
    - Include Swift preview for development

---
+++ SupabaseAPI/Views/MainViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MainViewController.swift
+//  SupabaseAPI
+//
+//  Created by Ali Osman Öztürk on 25.09.2025.
+//
+
+
+import UIKit
+
+class MainViewController: UIViewController {
+    private var welcomeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        setupConstraints()
+        setupNavigationBar()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        welcomeLabel = UILabel()
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        welcomeLabel.text = "Welcome to RESTfulAPI"
+        welcomeLabel.textAlignment = .center
+        welcomeLabel.font = .systemFont(ofSize: 24, weight: .bold)
+        view.addSubview(welcomeLabel)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            welcomeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    private func setupNavigationBar() {
+        navigationItem.title = "Main"
+        navigationController?.navigationBar.isHidden = false
+    }
+}
+
+#Preview {
+    MainViewController()
+}
